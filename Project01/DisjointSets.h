7ecd6007a4b42e16201c593834d84e4203b50bf3
Erez Cohen <erez6356458@gmail.com>
2022-04-09 18:08:23 +0300

    data structure
    Signed-off-by: Erez Cohen <erez6356458@gmail.com>

---
+++ Project01/DisjointSets.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Element.h"
+
+using std::vector;
+using std::endl;
+using std::cout;
+
+class DisjointSets
+{
+	Element* tree;
+	int treeSize;
+	int maxSize;
+	bool allocated;
+
+public:
+	DisjointSets();
+	void CreateEmpty(int max);
+	void MakeSset(int newRepresentative);
+	int Find(int element);
+	void Union(int representativeSetx, int representativeSety);
+	~DisjointSets();
+		void print() //need to be deleted for submission
+	{
+		for (int i = 0; i < treeSize; i++)
+		{
+			cout << "node: " << i << " parent: " << tree[i].parent << " size: " << tree[i].size << endl;
+		}
+		cout << "finished" << endl;
+	}
+};
+
---
+++ Project01/Pair.h
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef struct pair
+{
+	int key;
+	int data;
+
+} Pair;
