1edb3cb78b9fe61087d36d187be23aab918c12fd
Erez Cohen <erez6356458@gmail.com>
2022-04-09 23:54:41 +0300

    upload MST_algo

--- Project01/MinHeap.cpp
+++ Project01/MinHeap.cpp
@@ -153,13 +153,13 @@ bool MinHeap::isEmpty()
 
 void MinHeap::Build(int arr[], int size)
 {
+	Pair node;
+
 	CreateEmpty(size);
 
 	for (int i = 0; i < size; i++)
 	{
-		Pair node;
-
-		node.data = i;
+		node.data = i + 1;
 		node.key = arr[i];
 		data[i] = node;
 	}
